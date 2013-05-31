772cee753872a7367ab705377ac9c75ea22c23ac
huya.nzb <huya.nzb@taobao.com>
2013-05-31 18:15:07 +0800

    添加run/stop事件，checkScroll方法bugfix，demo修改

--- 1.0/demo/js/index.js
+++ 1.0/demo/js/index.js
@@ -7,37 +7,25 @@ S.use('node,sizzle,ajax,rich-base,gallery/scroll-monitor/1.0/,gallery/juicer/1.0
             this.template = Juicer(this.get('template'));
             this.resultFormatter = this.get('resultFormatter');
             this.pageNum = 0;
-            
-            this.initScrollMonitor();
         },
         
         destructor: function() {
             this.monitor.destroy()    
         },
         
-        initScrollMonitor: function() {
-            this.monitor = new ScrollMonitor({
-                node: 'body',
-                margin: 70
-            });
-            this.monitor.on('scrollToBottom', function(e) {
-                this.load();
-            }, this);
-        },
-        
-        run: function() {
-            this.monitor.run();
+        empty: function() {
+            this.abort();
+            this.pageNum = 0;
+            this.container.empty();
+            this.fire('empty');  
             return this;
         },
         
-        stop: function() {
-            this.monitor.stop();
-            return this;    
-        },
-        
-        add: function(r) {
+        add: function(r, l) {
             this.container.append(this.tpl(r));
-            this.fire('add');
+            this.fire('add', {
+                size: l
+            });
             return this;
         },
         
@@ -52,24 +40,49 @@ S.use('node,sizzle,ajax,rich-base,gallery/scroll-monitor/1.0/,gallery/juicer/1.0
             return html;
         },
         
+        setAPI: function(api, data) {
+            if (DribbbleLoader.APIS[api]) {
+                this.set('API', S.substitute(DribbbleLoader.APIS[api], data));
+            }
+            return this;
+        },
+        
+        abort: function() {
+            if (this._io) {
+                this._io.aborted = true;   
+                this._io = null;
+                this.fire('abort');
+            }
+            return this;
+        },
+        
         load: function() {
             var self = this,
-                url = this.get('API') + '?' + S.param({
+                url = DribbbleLoader.DOMAIN + this.get('API') + '?' + S.param({
                     'per_page': this.get('pageSize'),
                     'page': ++this.pageNum
-                });
+                }),
+                io = { aborted: false };
+            
+            this._io = io;
             
             S.jsonp(url, function(r) {
+                if (io.aborted) { return; }
                 if (r && r.shots && r.shots.length) {
-                    self.add(r);
+                    self.add(r, r.shots.length);
+                    if (Number(r.page) === r.pages) {
+                        self.fire('end');
+                    }
                 } else {
-                    if (r && (!r.shots || r.shots.length)) {
+                    if (r && (!r.shots || !r.shots.length)) {
                         self.fire('end');
                     } else {
                         self.fire('error');
                     }
                 }
             });
+            
+            this.fire('load');
             
             return this;
         }
@@ -93,24 +106,38 @@ S.use('node,sizzle,ajax,rich-base,gallery/scroll-monitor/1.0/,gallery/juicer/1.0
             },
             
             pageSize: {
-                getter: function(v) {
-                    return S.UA.mobile ? Math.ceil(v / 2) : v;    
-                },
                 value: 20
             },
             
             API: {
-                value: 'http://api.dribbble.com/shots/popular'
+                value: '/shots/popular'
             }
             
-        }
+        },
+        
+        APIS: {
+            list     : '/shots/{list}',
+            player   : '/players/{id}/shots',
+            following: '/players/{id}/shots/following',
+            likes    : '/players/{id}/shots/likes'
+        },
+        
+        DOMAIN: 'http://api.dribbble.com'
         
     });
     
+    var params = S.unparam(location.search.substring(1)),
+        type = params['type'] || 'popular';
+    
     var loader = new DribbbleLoader({
         container: '.db-list',
-        pageSize: 32,
+        pageSize: S.UA.mobile ? 16 : 32,
         template: S.one('#J_db_template').html()
+    });
+    
+    var monitor = new ScrollMonitor({
+        node: 'body',
+        margin: 70
     });
     
     var loadingText = S.one('.db-loading span'),
@@ -119,10 +146,6 @@ S.use('node,sizzle,ajax,rich-base,gallery/scroll-monitor/1.0/,gallery/juicer/1.0
     
     function updateLoadingText(text) {
         loadingText.html(text);
-    }
-    
-    function stopIconAnimation() {
-        loadingIcon.addClass('stop');
     }
     
     function bindImageLoad(img) {
@@ -136,32 +159,87 @@ S.use('node,sizzle,ajax,rich-base,gallery/scroll-monitor/1.0/,gallery/juicer/1.0
         }
     }
     
-    loader.on('add', function() {
+    monitor.on('scrollToBottom', function(e) {
+        loader.load();
+    });
+    
+    loader.on('add', function(e) {
         if (!added) {
             updateLoadingText('Loading More Shots...');
         }
         added = true;
-        var imgs = S.query('.db-item img'),
+        var imgs = S.query('.db-link img'),
             i = imgs.length - 1,
-            e = i - 32;
+            end = i - e.size;
             
-        for (; i > e; i--) {
+        for (; i > end; i--) {
             imgs[i] && bindImageLoad(imgs[i]);
         }
+
+        monitor.checkScroll(true);
+    });
+    
+    loader.on('empty', function() {
+        updateLoadingText('Loading Shots...');
+        added = false;
+    });
+    
+    loader.on('load', function(e) {
+        loadingIcon.removeClass('stop');
     });
     
     loader.on('end', function() {
         updateLoadingText('No More Shots.');
-        stopIconAnimation();
-        this.stop();
+        loadingIcon.addClass('stop');
+        monitor.stop();
     });
     
     loader.on('error', function() {
         updateLoadingText('Loading Error!');
-        stopIconAnimation();
-        this.stop();
+        loadingIcon.addClass('stop');
+        monitor.stop();
     });
     
-    loader.run();
+    monitor.run();
+    
+    var dbList = S.one('.db-list'),
+        tabs = S.one('.tabs'),
+        tabItems = tabs.all('li'),
+        playerTabs = tabs.all('.player');
+    
+    function load(target) {
+        var api = target.attr('data-api'),
+            cfg = target.attr('data-cfg').split(':'),
+            data = {};
+        
+        data[cfg[0]] = cfg[1];
+        monitor.stop();
+        loader.empty();
+        loader.setAPI(api, data);
+        monitor.run();
+    }
+    
+    tabs.delegate('click', 'li', function(e) {
+        var target = S.one(e.currentTarget);
+        if (!target.hasClass('active')) {
+            tabItems.removeClass('active');
+            target.addClass('active');
+            if (!target.hasClass('player')) {
+                 playerTabs.addClass('hidden');
+            }
+            load(target);
+        }
+    });
+    
+    dbList.delegate('click', '.db-over', function(e) {
+        var target = S.one(e.currentTarget).one('strong');
+        e.halt();
+        tabItems.removeClass('active');
+        playerTabs.item(0).addClass('active');
+        playerTabs.item(0).html(target.attr('data-player'));
+        playerTabs.removeClass('hidden');
+        playerTabs.attr('data-cfg', 'id:' + target.attr('data-id'));
+        load(target);
+    });
         
 });
